1710cba040695b0c1055458b3d0c9fbf498c9102
giulia shelton <giuliashelton6875@gmail.com>
2022-11-02 12:19:43 -0500

    I have moved all of my folders into the password-generator folder because that folder is connected to my github

--- Develop/script.js
+++ Develop/script.js
@@ -1,86 +1,161 @@
-
-
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var lowercaseChar = "abcdefghijklmnopqrstuvwxyz";
+var uppercaseChar = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numberChar = "0123456789";
+var specialChar = "!@#$%^&*()_-+={}[];:'`~<,>.?/|"
+var passwordLength;
+var uppercaseCheck;
+var numberCheck;
+var specialCheck;
 
+//Function used to determine the length of the password
+function determineLength(){
+  passwordLength = prompt("Choose how many characters long you'd like your password to be (between 8-128 characters): ");
 
-// Created Arrays of Possible Character Choices
-var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var special = ['@', '%', '+', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'
-];
-
-// Created function to ask user which options and made conditional statements to ensure minimum option requirements were met.
-function questions() {
-  var isValid = false;
-  do {
-    var length = prompt("Choose password length between 8 and 128 characters");
-    var askNumbers = confirm("Do you want your password to include numbers?");
-    var askLowerCase = confirm("Do you want your password to include lower case letters?");
-    var askUpperCase = confirm("Do you want your password to include upper case letters?");
-    var askSpecial = confirm("Do you want your password to include special characters?");
-    var responses = {
-      length: length,
-      askNumbers: askNumbers,
-      askLowerCase: askLowerCase,
-      askUpperCase: askUpperCase,
-      askSpecial: askSpecial
-    } 
-    if((length < 8)||(length > 128))
-    alert("Choose number between 8 and 128");
-    else if((!askNumbers)&&(!askLowerCase)&&(!askUpperCase)&&(!askSpecial))
-    alert("Must choose at least one type.");
-    else
-    isValid = true;
-
-  } while(!isValid);
-  return responses;
+    if (passwordLength<8){
+      alert("Password length must be a number between 8-128 characters");
+      determineLength();
+    }else if (passwordLength>128){
+      alert("Password length must be a number between 8-128 characters");
+      determineLength();
+    }else if (isNaN(passwordLength)){
+      alert("Password length must be a number between 8-128 characters");
+      determineLength();
+    }else{
+    alert("The next three screens will ask you what types of characters you would like to be included in your password.\nIf you choose 'No' for all, your password will only contain lowercase letters.");
+    }
+    return passwordLength;
 }
-// This function joins all the user responses and then creates the result - a strong password.
-function generatePassword() {
-  var passwordOptions = questions();
-  var possibleCombo = [];
-  var finalPassword = "";
 
+//Function used to determine whether the user wants to include uppercase characters in the password
+function determineUppercase(){
+  uppercaseCheck = prompt("Do you want to include uppercase letters in your password? \n(Yes or No)");
+    uppercaseCheck = uppercaseCheck.toLowerCase();
 
+    if (uppercaseCheck === null || uppercaseCheck === ""){
+      alert("Please answer Yes or No");
+      determineUppercase();
 
-  if (passwordOptions.askNumbers) {
-    for (var i of numbers)
-      possibleCombo.push(i);
-  }
-  if (passwordOptions.askLowerCase) {
-    for (var i of lowerCase)
-      possibleCombo.push(i);
-  }
-  if (passwordOptions.askUpperCase) {
-    for (var i of upperCase)
-      possibleCombo.push(i);
-  }
-  if (passwordOptions.askSpecial) {
-    for (var i of special)
-      possibleCombo.push(i);
-  }
+    }else if (uppercaseCheck === "yes" || uppercaseCheck ==="y"){
+      uppercaseCheck = true;
+      return uppercaseCheck;
 
-
-  console.log(possibleCombo);
-
-
-  for (var i = 0; i < passwordOptions.length; i++) {
-    finalPassword += possibleCombo[Math.floor(Math.random() * possibleCombo.length)];
+    }else if (uppercaseCheck === "no" || uppercaseCheck ==="n"){
+      uppercaseCheck = false;
+      return uppercaseCheck;
     
-  }
-  console.log(finalPassword);
+    }else {
+      alert("Please answer Yes or No");
+      determineUppercase();
+    }
+    return uppercaseCheck;
+}
 
-  return finalPassword;
+//Function used to determine whether the user wants to include numbers in the password
+function determineNumbers(){
+  numberCheck = prompt("Do you want to include numbers in your password? \n(Yes or No)");
+    numberCheck = numberCheck.toLowerCase();
+
+    if (numberCheck === null || numberCheck === ""){
+      alert("Please answer Yes or No");
+      determineNumbers();
+
+    }else if (numberCheck === "yes" || numberCheck ==="y"){
+      numberCheck = true;
+      return numberCheck;
+
+    }else if (numberCheck === "no" || numberCheck ==="n"){
+      numberCheck = false;
+      return numberCheck;
+    
+    }else {
+      alert("Please answer Yes or No");
+      determineNumbers();
+    }
+    return numberCheck;
+}
+
+//Function used to determine whether the user wants to include special characters in the password
+function determineSpecial(){
+  specialCheck = prompt("Do you want to include special characters in your password? \n(Yes or No)");
+    specialCheck = specialCheck.toLowerCase();
+
+    if (specialCheck === null || specialCheck === ""){
+      alert("Please answer Yes or No");
+      determineSpecial();
+
+    }else if (specialCheck === "yes" || specialCheck ==="y"){
+      specialCheck = true;
+      return specialCheck;
+
+    }else if (specialCheck === "no" || specialCheck ==="n"){
+      specialCheck = false;
+      return specialCheck;
+    
+    }else {
+      alert("Please answer Yes or No");
+      determineSpecial();
+    }
+    return specialCheck;
+}
+
+ //Function used to take all the input from the previous functions and generate a password using a random number generator and 
+ //the charAt method 
+function generatePassword(){
+  determineLength();
+  console.log(passwordLength);
+  determineUppercase();
+  console.log(uppercaseCheck);
+  determineNumbers();
+  console.log(numberCheck);
+  determineSpecial();
+  console.log(specialCheck);
+
+var characters = lowercaseChar;
+var password = "";
+if (uppercaseCheck && numberCheck && specialCheck){
+  characters += uppercaseChar + numberChar + specialChar;
+
+}else if (uppercaseCheck && numberCheck){
+  characters += uppercaseChar + numberChar;
+
+}else if (numberCheck && specialCheck){
+  characters += numberChar + specialChar;
+
+}else if (uppercaseCheck && specialCheck){
+  characters += uppercaseChar + specialChar;
+
+}else if (uppercaseCheck){
+  characters += uppercaseChar;
+
+}else if(numberCheck){
+  characters += numberChar;
+
+}else if (specialCheck){
+  characters += specialChar;
+
+}else{
+  characters === lowercaseChar;
+}
+
+  for(var i = 0; i < passwordLength; i++){
+    password += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return password;
 }
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password1 = "";
+  password1 = generatePassword();
   var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
+  passwordText.value = password1;
 }
+
+// function resetText(){
+//   document.getElementById("password").value = "Your Secure Password";
+// }
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Password-Generator
-Java Challenge
+For this activity 
+
+
+![image](https://user-images.githubusercontent.com/114687261/199322556-648eeef1-7f99-4a3f-ba63-d3f62915f008.png)
